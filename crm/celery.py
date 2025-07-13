833cb964ed1f30dcdfa8a2c08e1518df0ea8a7e5
Tsigie Beyene <119432900+Tsigie-beyene@users.noreply.github.com>
2025-07-13 10:21:47 -0700

    Update celery.py

--- crm/celery.py
+++ crm/celery.py
@@ -1,9 +1,14 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crm.settings')
+# Set default Django settings module
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alx-backend-graphql_crm.settings')
 
-app = Celery('crm', broker='redis://localhost:6379/0')
+# Initialize Celery
+app = Celery('crm')
 
+# Load settings from Django config using 'CELERY_' namespace
 app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Auto-discover tasks in all installed apps
 app.autodiscover_tasks()
